255617e4fcc21a306157c989a921a298883affe0
Juan Dorado <jfdoradotr@icloud.com>
2025-08-25 19:18:07 -0600

    `FetchLatestPrice` propagates loader error

--- btc-price/BTCPriceCore/Tests/BTCPriceCoreTests/BTCPriceCoreTests.swift
+++ btc-price/BTCPriceCore/Tests/BTCPriceCoreTests/BTCPriceCoreTests.swift
@@ -7,6 +7,8 @@ struct LoaderStub: PriceLoader {
   func loadLatest() async throws -> PriceQuote { try result.get() }
 }
 
+enum DummyError: Error { case any }
+
 @Test func fetchLatestPrice_deliversValueFromLoader() async throws {
   let expected = PriceQuote(
     value: 68_901.23,
@@ -19,4 +21,13 @@ struct LoaderStub: PriceLoader {
   let received = try await sut.execute()
 
   #expect(received == expected)
+}
+
+@Test
+func fetchLatestPrice_propagatesLoaderError() async {
+  let sut = FetchLatestPrice(loader: LoaderStub(result: .failure(DummyError.any)))
+
+  await #expect(throws: Error.self) {
+    _ = try await sut.execute()
+  }
 }
